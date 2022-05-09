cb6679bc7055dd88e3cc0e12be61b24bf939a5e0
juvilla <92930948+juvilla@users.noreply.github.com>
2022-05-09 16:43:09 -0500

    Add files via upload

---
+++ src/main/java/Camion.java
@@ -0,0 +1,32 @@
+package vehiculos;
+
+public class Camion extends Vehiculo {
+	private int ejes;
+	public static int CantidadCamion;
+	public Camion(String placa,String nombre,int precio,int peso,Fabricante fabricante,int ejes){
+		super();
+		setPlaca(placa);
+		setPuertas(2);
+		setVelocidadMaxima(80);
+		setNombre(nombre);
+		setPrecio(precio);
+		setPeso(peso);
+		setTraccion("4X2");
+		setFabricante(fabricante);
+		setEjes(ejes);
+		CantidadCamion++;
+	}
+	public int getEjes() {
+		return ejes;
+	}
+	public void setEjes(int ejes) {
+		this.ejes=ejes;
+	}
+	public static int getCantidadCamion() {
+		return CantidadCamion;
+	}
+	public static void setCantidadCamion(int Cc) {
+		CantidadCamion=Cc;
+	}
+
+}
