ca2bc90af121255b3f6cd6b8917579a52c7b7ff7
juvilla <92930948+juvilla@users.noreply.github.com>
2022-05-09 16:18:34 -0500

    Add files via upload

--- src/main/java/vehiculos/Fabricante.java
+++ src/main/java/vehiculos/Fabricante.java
@@ -3,16 +3,20 @@ package vehiculos;
 public class Fabricante {
 	private String nombre;
 	private Pais pais;
-	String getNombre() {
+	Fabricante(String nombre,Pais pais){
+		this.nombre=nombre;
+		this.pais=pais;
+	}
+	public String getNombre() {
 		return nombre;
 	}
-	void setNombre(String nombre) {
+	public void setNombre(String nombre) {
 		this.nombre=nombre;
 	}
-	Pais getPais() {
+	public Pais getPais() {
 		return pais;
 	}
-	void setPais(Pais pais) {
+	public void setPais(Pais pais) {
 		this.pais=pais;
 	}
 }
--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -2,6 +2,9 @@ package vehiculos;
 
 public class Pais {
 	private String nombre;
+	Pais(String nombre){
+		this.nombre=nombre;
+	}
 	String getNombre(){
 		return nombre;
 	}
